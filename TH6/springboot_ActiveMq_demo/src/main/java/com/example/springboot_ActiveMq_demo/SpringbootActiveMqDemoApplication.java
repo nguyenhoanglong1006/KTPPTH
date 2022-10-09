fae918e528f02a0d474f525e5eef1f477c42e7f9
Nguyễn Phạm Hoàng Long <84476891+nguyenhoanglong1006@users.noreply.github.com>
2022-10-09 14:27:46 +0700

    first commit

---
+++ TH6/springboot_ActiveMq_demo/src/main/java/com/example/springboot_ActiveMq_demo/SpringbootActiveMqDemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.springboot_ActiveMq_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+
+@SpringBootApplication
+public class SpringbootActiveMqDemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(
+				SpringbootActiveMqDemoApplication.class, args);
+		
+		JmsTemplate jms = ctx.getBean(JmsTemplate.class);
+		jms.convertAndSend("javainuse", "Nguyen Pham Hoang Long");
+	}
+
+
+}
