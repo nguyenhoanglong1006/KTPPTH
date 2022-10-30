f473d18ba950a17a5ed4b1590b631e40b16ddbf5
Nguyễn Phạm Hoàng Long <84476891+nguyenhoanglong1006@users.noreply.github.com>
2022-10-30 14:52:34 +0700

    first commit

---
+++ TH_8/EurekaClient2/src/main/java/com/example/EurekaClient2/EurekaClient2Application.java
@@ -0,0 +1,21 @@
+package com.example.EurekaClient2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaClient2Application {
+   public static void main(String[] args) {
+      SpringApplication.run(EurekaClient2Application.class, args);
+   }
+   @RequestMapping(value = "/")
+   public String home() {
+      return "Eureka Client application";
+   }
+}
+
